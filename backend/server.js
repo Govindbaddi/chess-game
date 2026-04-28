661cb57b072fe8920d65b1cf6432b184e2cae6bf
GovindBaddi <baddigovindarao710@gmail.com>
2026-04-28 14:53:30 +0530

    client url added

--- backend/server.js
+++ backend/server.js
@@ -15,11 +15,12 @@ const parser = require("./utilities/uploads")
 const {Game}=require('./models/gamemodel')
 
 const app=express()
+const CLIENT_URL=process.env.CLIENT_URL
 //middle wares
 app.use(express.json())
 
 app.use(cors({
-    origin:["http://localhost:5173"],
+    origin:[CLIENT_URL || "http://localhost:5173"],
     credentials:true
 }))
 app.use(cookieParser())
@@ -71,7 +72,7 @@ const MONGODB_URI=process.env.MONGODB_URI
 
 const server=http.createServer(app);
 const io=new Server(server,{cors:{
-    origin:["http://localhost:5173"],
+    origin:[ CLIENT_URL || "http://localhost:5173"],
     credentials:true
 }})
 
